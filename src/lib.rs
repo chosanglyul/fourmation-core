a0a5c8231bf5b86d29ce9a1c075dd3577918d9ef
HoseongLee <leehs.git@gmail.com>
2025-08-15 09:52:48 +0000

    feat: bitboard representation (WIP)

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,288 @@ pub const MAXX: usize = 7;
 pub const MAXY: usize = 7;
 pub const FOUR: usize = 4;
 
+use std::fmt;
+use std::ops::*;
+
+pub type Position = u64;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Board(u64);
+
+impl Board {
+    pub fn new(value: u64) -> Self {
+        assert!(
+            value < 0x2000000000000,
+            "BitBoard must be smaller than 2^49"
+        );
+
+        Board(value)
+    }
+
+    pub fn singleton(position: Position) -> Self {
+        Board(1 << position)
+    }
+
+    pub fn get(&self, index: Position) -> bool {
+        self.0 & (1 << index) != 0
+    }
+
+    pub fn set(&self, index: Position) -> Self {
+        Board(self.0 | (1 << index))
+    }
+
+    pub fn up(&self) -> Self {
+        Board(self.0 >> 7)
+    }
+
+    pub fn down(&self) -> Self {
+        Board(self.0 << 7)
+    }
+
+    pub fn left(&self) -> Self {
+        Board((self.0 >> 1) & 0xFDFBF7EFDFBF)
+    }
+
+    pub fn right(&self) -> Self {
+        Board((self.0 << 1) & 0x1FBF7EFDFBF7E)
+    }
+
+    pub fn neighbors(&self) -> Self {
+        let up = self.up();
+        let down = self.down();
+
+        up | down | self.left() | self.right() | up.left() | up.right() | down.left() | down.right()
+    }
+}
+
+impl BitAnd for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitand(self, other: Board) -> Board {
+        Board(self.0 & other.0)
+    }
+}
+
+impl BitAnd for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitand(self, other: &Board) -> Board {
+        Board(self.0 & other.0)
+    }
+}
+
+impl BitAnd<&Board> for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitand(self, other: &Board) -> Board {
+        Board(self.0 & other.0)
+    }
+}
+
+impl BitAnd<Board> for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitand(self, other: Board) -> Board {
+        Board(self.0 & other.0)
+    }
+}
+
+impl BitOr for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitor(self, other: Board) -> Board {
+        Board(self.0 | other.0)
+    }
+}
+
+impl BitOr for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitor(self, other: &Board) -> Board {
+        Board(self.0 | other.0)
+    }
+}
+
+impl BitOr<&Board> for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitor(self, other: &Board) -> Board {
+        Board(self.0 | other.0)
+    }
+}
+
+impl BitOr<Board> for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitor(self, other: Board) -> Board {
+        Board(self.0 | other.0)
+    }
+}
+
+impl BitXor for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitxor(self, other: Board) -> Board {
+        Board(self.0 ^ other.0)
+    }
+}
+
+impl BitXor for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitxor(self, other: &Board) -> Board {
+        Board(self.0 ^ other.0)
+    }
+}
+
+impl BitXor<&Board> for Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitxor(self, other: &Board) -> Board {
+        Board(self.0 ^ other.0)
+    }
+}
+
+impl BitXor<Board> for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn bitxor(self, other: Board) -> Board {
+        Board(self.0 ^ other.0)
+    }
+}
+
+impl BitAndAssign for Board {
+    #[inline]
+    fn bitand_assign(&mut self, other: Board) {
+        self.0 &= other.0;
+    }
+}
+
+impl BitAndAssign<&Board> for Board {
+    #[inline]
+    fn bitand_assign(&mut self, other: &Board) {
+        self.0 &= other.0;
+    }
+}
+
+impl BitOrAssign for Board {
+    #[inline]
+    fn bitor_assign(&mut self, other: Board) {
+        self.0 |= other.0;
+    }
+}
+
+impl BitOrAssign<&Board> for Board {
+    #[inline]
+    fn bitor_assign(&mut self, other: &Board) {
+        self.0 |= other.0;
+    }
+}
+
+impl BitXorAssign for Board {
+    #[inline]
+    fn bitxor_assign(&mut self, other: Board) {
+        self.0 ^= other.0;
+    }
+}
+
+impl BitXorAssign<&Board> for Board {
+    #[inline]
+    fn bitxor_assign(&mut self, other: &Board) {
+        self.0 ^= other.0;
+    }
+}
+
+impl Mul for Board {
+    type Output = Board;
+
+    #[inline]
+    fn mul(self, other: Board) -> Board {
+        Board(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn mul(self, other: &Board) -> Board {
+        Board(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul<&Board> for Board {
+    type Output = Board;
+
+    #[inline]
+    fn mul(self, other: &Board) -> Board {
+        Board(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul<Board> for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn mul(self, other: Board) -> Board {
+        Board(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Not for Board {
+    type Output = Board;
+
+    #[inline]
+    fn not(self) -> Board {
+        Board(!self.0)
+    }
+}
+
+impl Not for &Board {
+    type Output = Board;
+
+    #[inline]
+    fn not(self) -> Board {
+        Board(!self.0)
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut s = "".to_owned();
+
+        for i in 0..49 {
+            s.push_str(if self.0 & (1 << i) == 0 { "X " } else { "O " });
+
+            if i % 7 == 6 {
+                s.push('\n');
+            }
+        }
+
+        write!(f, "{}", s)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Player {
     R,
     B,
 }
 
-impl std::fmt::Display for Player {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Player::R => write!(f, "R"),
             Player::B => write!(f, "B"),
@@ -18,7 +292,7 @@ impl std::fmt::Display for Player {
 }
 
 impl Player {
-    pub fn get_opponent(&self) -> Self {
+    pub fn opponent(&self) -> Self {
         match self {
             Player::R => Player::B,
             Player::B => Player::R,
@@ -26,276 +300,117 @@ impl Player {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Translation {
-    x: isize,
-    y: isize,
-}
-
-impl Translation {
-    pub fn from_coordinate(x: isize, y: isize) -> Self {
-        Self { x, y }
-    }
-
-    pub fn scalar_multiplication(&self, c: isize) -> Self {
-        Self {
-            x: self.x * c,
-            y: self.y * c,
-        }
-    }
-
-    pub fn from_difference(a: Position, b: Position) -> Self {
-        Self {
-            x: a.x as isize - b.x as isize,
-            y: a.y as isize - b.y as isize,
-        }
-    }
-
-    pub fn is_unit(&self) -> bool {
-        if self.x == 0 && self.y == 0 {
-            false
-        } else if self.x.unsigned_abs() <= 1 && self.y.unsigned_abs() <= 1 {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Position {
-    x: usize,
-    y: usize,
-}
-
-impl std::fmt::Display for Position {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-impl Position {
-    pub fn from_coordinate(x: usize, y: usize) -> Option<Self> {
-        if x < MAXX && y < MAXY {
-            Some(Self { x, y })
-        } else {
-            None
-        }
-    }
-
-    pub fn add_translation(&self, translation: Translation) -> Option<Self> {
-        Self::from_coordinate(
-            self.x.checked_add_signed(translation.x)?,
-            self.y.checked_add_signed(translation.y)?,
-        )
-    }
-
-    pub fn all_neighbor(&self) -> Vec<Self> {
-        [
-            (0, 1),
-            (1, 0),
-            (1, 1),
-            (1, -1),
-            (0, -1),
-            (-1, 0),
-            (-1, -1),
-            (-1, 1),
-        ]
-        .into_iter()
-        .map(|(x, y)| Translation::from_coordinate(x, y))
-        .map(|translation| self.add_translation(translation))
-        .flatten()
-        .collect()
-    }
-
-    pub fn to_usize(&self) -> usize {
-        self.x * MAXY + self.y
-    }
-
-    pub fn all_position() -> Vec<Self> {
-        (0..MAXX)
-            .into_iter()
-            .map(|x| (0..MAXY).into_iter().map(move |y| (x, y)))
-            .flatten()
-            .map(|(x, y)| Position::from_coordinate(x, y).unwrap())
-            .collect()
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy)]
 pub struct Action {
     pub player: Player,
     pub position: Position,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct State {
-    pub board: Vec<Option<Player>>,
-    pub last_move: Option<Action>,
-}
-
-impl std::fmt::Display for State {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for x in 0..MAXX {
-            for y in 0..MAXY {
-                let pos = Position::from_coordinate(x, y).unwrap();
-                match self[pos] {
-                    Some(player) => {
-                        if self
-                            .last_move
-                            .is_some_and(|last_move| last_move.position == pos)
-                        {
-                            write!(f, "{}* ", player)?
-                        } else {
-                            write!(f, "{}. ", player)?
-                        }
-                    }
-                    None => write!(f, ".. ")?,
-                }
-            }
-            writeln!(f)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl std::ops::Index<Position> for State {
-    type Output = Option<Player>;
-
-    fn index(&self, index: Position) -> &Self::Output {
-        &self.board[index.to_usize()]
-    }
+    pub r_board: Board,
+    pub b_board: Board,
+    pub previous_action: Option<Action>,
 }
 
 impl State {
     pub fn from_empty() -> Self {
         State {
-            board: Position::all_position().iter().map(|_| None).collect(),
-            last_move: None,
+            r_board: Board(0),
+            b_board: Board(0),
+            previous_action: None,
         }
     }
 
-    pub fn get_next_player(&self) -> Player {
-        match self.last_move {
-            Some(action) => action.player.get_opponent(),
+    pub fn player(&self) -> Player {
+        match self.previous_action {
+            Some(action) => action.player.opponent(),
             None => Player::R,
         }
     }
 
-    fn get_next_state(&self, next_move: &Action) -> Option<Self> {
-        if self.get_next_player() != next_move.player
-            || self[next_move.position].is_some()
-            || self.last_move.is_some_and(|last_move| {
-                if next_move
-                    .position
-                    .all_neighbor()
-                    .into_iter()
-                    .all(|pos| self[pos] != Some(last_move.player))
-                {
-                    true
-                } else if Translation::from_difference(last_move.position, next_move.position)
-                    .is_unit()
-                {
-                    false
-                } else {
-                    last_move
-                        .position
-                        .all_neighbor()
-                        .into_iter()
-                        .any(|pos| self[pos].is_none())
-                }
-            })
-        {
-            None
-        } else {
-            Some(State {
-                board: self
-                    .board
-                    .iter()
-                    .enumerate()
-                    .map(|(idx, &player)| {
-                        if idx == next_move.position.to_usize() {
-                            Some(next_move.player)
-                        } else {
-                            player
-                        }
-                    })
-                    .collect(),
-                last_move: Some(*next_move),
-            })
+    fn is_empty(&self, position: Position) -> bool {
+        self.r_board.get(position) || self.b_board.get(position)
+    }
+
+    // TODO: Change Option to Result
+    pub fn step(&self, action: &Action) -> Option<(Self, bool)> {
+        if self.player() != action.player || self.is_empty(action.position) {
+            return None;
         }
-    }
 
-    fn check_win(&self) -> bool {
-        let last_player = self.last_move.unwrap().player;
-        let last_position = self.last_move.unwrap().position;
-
-        [(0, 1), (1, 0), (1, 1), (1, -1)]
-            .into_iter()
-            .map(|(x, y)| Translation::from_coordinate(x, y))
-            .any(|translation| {
-                (-3..=3)
-                    .into_iter()
-                    .map(|c| translation.scalar_multiplication(c))
-                    .map(|dir| last_position.add_translation(dir))
-                    .flatten()
-                    .map(|pos| self[pos])
-                    .collect::<Vec<_>>()
-                    .windows(FOUR)
-                    .any(|arr| arr.iter().all(|&player| player == Some(last_player)))
-            })
-    }
-
-    pub fn fourmation_turn(&self, next_move: &Action) -> Option<NextState> {
-        let new_state = self.get_next_state(next_move)?;
-
-        if new_state.check_win() {
-            Some(NextState::Done(Some(next_move.player)))
-        } else if new_state.get_next_position().is_empty() {
-            Some(NextState::Done(None))
-        } else {
-            Some(NextState::Cont(new_state))
+        if !self.previous_action.is_some_and(|previous_action| {
+            Board::singleton(previous_action.position)
+                .neighbors()
+                .get(action.position)
+        }) {
+            return None;
         }
+
+        let (r_board, b_board) = match action.player {
+            Player::R => (self.r_board.set(action.position), self.b_board),
+            Player::B => (self.r_board, self.b_board.set(action.position)),
+        };
+
+        let new_state = State {
+            r_board,
+            b_board,
+            previous_action: Some(*action),
+        };
+
+        Some((new_state, new_state.is_done()))
     }
 
-    pub fn get_next_position(&self) -> Vec<Position> {
-        if let Some(last_move) = self.last_move {
-            let near_last: Vec<Position> = last_move
-                .position
-                .all_neighbor()
-                .into_iter()
-                .filter(|&pos| self[pos].is_none())
-                .collect();
+    fn is_done(&self) -> bool {
+        if let Some(previous_action) = self.previous_action {
+            let last_position = previous_action.position;
 
-            if near_last.is_empty() {
-                Position::all_position()
-                    .into_iter()
-                    .filter(|&pos| {
-                        self[pos].is_none()
-                            && pos
-                                .all_neighbor()
-                                .into_iter()
-                                .any(|near_pos| self[near_pos] == Some(last_move.player))
-                    })
-                    .collect()
-            } else {
-                near_last
-            }
-        } else {
-            Position::all_position()
+            let (cp_board, op_board) = match previous_action.player {
+                Player::R => (self.r_board, self.b_board),
+                Player::B => (self.b_board, self.r_board),
+            };
         }
+
+        false
     }
-}
 
-pub enum NextState {
-    Done(Option<Player>),
-    Cont(State),
-}
+    // pub fn fourmation_turn(&self, next_move: &Action) -> Option<NextState> {
+    //     let new_state = self.get_next_state(next_move)?;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    //     if new_state.check_win() {
+    //         Some(NextState::Done(Some(next_move.player)))
+    //     } else if new_state.get_next_position().is_empty() {
+    //         Some(NextState::Done(None))
+    //     } else {
+    //         Some(NextState::Cont(new_state))
+    //     }
+    // }
 
-    #[test]
-    fn it_works() {}
+    // pub fn get_next_position(&self) -> Vec<Position> {
+    //     if let Some(previous_action) = self.previous_action {
+    //         let near_last: Vec<Position> = previous_action
+    //             .position
+    //             .all_neighbor()
+    //             .into_iter()
+    //             .filter(|&pos| self[pos].is_none())
+    //             .collect();
+
+    //         if near_last.is_empty() {
+    //             Position::all_position()
+    //                 .into_iter()
+    //                 .filter(|&pos| {
+    //                     self[pos].is_none()
+    //                         && pos
+    //                             .all_neighbor()
+    //                             .into_iter()
+    //                             .any(|near_pos| self[near_pos] == Some(previous_action.player))
+    //                 })
+    //                 .collect()
+    //         } else {
+    //             near_last
+    //         }
+    //     } else {
+    //         Position::all_position()
+    //     }
+    // }
 }
